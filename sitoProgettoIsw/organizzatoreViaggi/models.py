ac63ede516ff179dbe995ba087019ba321f398e4
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-09-09 19:57:46 +0200

    Iniziate implementazioni varie
    Iniziata l'implementazione della app organizzatoreViaggi e della pagina di login, dei modelli, delle views, delle urls etc.

---
+++ sitoProgettoIsw/organizzatoreViaggi/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    nome = models.CharField(max_length=20)
+    cognome = models.CharField(max_length=20)
+    password = models.CharField(max_length=32)
+    email = models.CharField(max_length=20)
