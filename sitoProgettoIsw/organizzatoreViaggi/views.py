baba8ad1ded281f82fc9bc7d11192579e1516323
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-09-11 01:17:20 +0200

    Implementato form di registrazione

--- sitoProgettoIsw/organizzatoreViaggi/views.py
+++ sitoProgettoIsw/organizzatoreViaggi/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 # Create your views here.
 
@@ -6,7 +8,15 @@ def login_view(request):
     return render(request, 'organizzatoreViaggi/login.html')
 
 def signup_view(request):
-    return render(request, 'organizzatoreViaggi/signup.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'organizzatoreViaggi/signup.html', context)
 
 def userHomePage_view(request):
     return render(request, 'organizzatoreViaggi/userHomePage.html')
