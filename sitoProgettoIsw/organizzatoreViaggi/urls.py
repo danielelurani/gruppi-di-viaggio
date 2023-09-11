b51bf404e556a3ded76dbd9d39eefbe82a2bff48
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-09-11 13:58:03 +0200

    Implementato sistema di autenticazione

--- sitoProgettoIsw/organizzatoreViaggi/urls.py
+++ sitoProgettoIsw/organizzatoreViaggi/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
+    path("logout/", views.logout_user, name='logout'),
     path('organizzatoreViaggi/signup', views.signup_view, name='signup'),
     path('organizzatoreViaggi/userHomePage', views.userHomePage_view, name='userHomePage')
 ]
--- sitoProgettoIsw/organizzatoreViaggi/views.py
+++ sitoProgettoIsw/organizzatoreViaggi/views.py
@@ -1,22 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
 def login_view(request):
-    return render(request, 'organizzatoreViaggi/login.html')
+
+    if request.user.is_authenticated:
+        return redirect('userHomePage')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username = username, password = password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('/organizzatoreViaggi/userHomePage')
+            else:
+                messages.info(request, 'Username o password errati!')
+
+        context = {}
+        return render(request, 'organizzatoreViaggi/login.html', context)
 
 def signup_view(request):
-    form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
+    if request.user.is_authenticated:
+        return redirect('userHomePage')
+    else:
+        form = CreateUserForm()
+ 
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
 
-    context = {'form': form}
-    return render(request, 'organizzatoreViaggi/signup.html', context)
+        context = {'form': form}
+        return render(request, 'organizzatoreViaggi/signup.html', context)
 
+@login_required(login_url = 'login')
 def userHomePage_view(request):
     return render(request, 'organizzatoreViaggi/userHomePage.html')
