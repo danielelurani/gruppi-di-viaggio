ca3573f03ae8fd6cbd2711a1511ee7f631360cd5
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-09-14 11:59:05 +0200

    Creati Models rimanenti
    Creati i models per Invito, Commento e Tappa

--- sitoProgettoIsw/organizzatoreViaggi/models.py
+++ sitoProgettoIsw/organizzatoreViaggi/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=30, null=False, primary_key=True)
-    email = models.EmailField(max_length=20, unique=True)
+    username = models.CharField(max_length=32, null=False, primary_key=True)
+    email = models.EmailField(max_length=64, unique=True)
 
 class Travel(models.Model):
     id = models.AutoField(primary_key=True)
@@ -17,3 +17,32 @@ class Travel(models.Model):
 
     def __str__ (self):
         return self.name
+
+class Stage(models.Model):
+    id = models.AutoField(primary_key=True)
+    travel = models.ForeignKey(Travel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    date = models.DateTimeField()
+
+    def __str__ (self):
+        return self.name
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(max_length=250)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    travel = models.ForeignKey(Travel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Comment id " + str(self.id)
+    
+class Invitation(models.Model):
+    id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    receiver = models.EmailField(max_length=64)
+    Travel = models.ForeignKey(Travel, on_delete=models.CASCADE)
+    state = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Invitation id " + str(self.id)
+
