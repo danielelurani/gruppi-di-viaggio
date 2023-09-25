97493b3bdfbb5c4d6b67bfb475cb0d324a3e3133
Geraf25 <f.sanna120@studenti.unica.it>
2023-09-25 19:56:42 +0200

    Aggiornati test
    manca modificare qualche nome nei siti poi i test legati a commenti e spesa

--- sitoProgettoIsw/organizzatoreViaggi/tests/ta_travel.py
+++ sitoProgettoIsw/organizzatoreViaggi/tests/ta_travel.py
@@ -2,6 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import selenium.webdriver.support.select
 
 
 def mytravel(driver):
@@ -31,3 +32,83 @@ def create_travel(driver):
     elem.send_keys(Keys.RETURN)
 
     time.sleep(3)
+
+def details_travel(driver):
+    elem = driver.find_element(By.XPATH, "//a[3]")
+    elem.click()
+
+    time.sleep(1)
+
+def modify_travel(driver):
+    elem = driver.find_element(By.XPATH, "//a[3]")
+    elem.click()
+
+    time.sleep(1)
+
+def form_modify_travel(driver):
+    elem = driver.find_element(By.NAME, "name")
+    elem.clear()
+    elem.send_keys("Modifica")
+
+    elem = driver.find_element(By.NAME, "destination")
+    elem.clear()
+    elem.send_keys("Modifica")
+
+    elem = driver.find_element(By.NAME, "start_date")
+    elem.clear()
+    elem.send_keys("2023-01-10")
+
+    elem = driver.find_element(By.NAME, "end_date")
+    elem.clear()
+    elem.send_keys("2023-04-02")
+
+    elem = driver.find_element(By.NAME, "edit_travel")
+    elem.send_keys(Keys.RETURN)
+
+    time.sleep(1)
+
+def form_create_location(driver):
+    elem = driver.find_element(By.NAME, "name")
+    elem.clear()
+    elem.send_keys("Tappa")
+
+    elem = driver.find_element(By.NAME, "description")
+    elem.clear()
+    elem.send_keys("Descrizizone luogo")
+
+    elem = driver.find_element(By.NAME, "date")
+    elem.clear()
+    elem.send_keys("2023-02-25")
+
+    time.sleep(1)
+
+def form_delete_location(driver):
+    elem = driver.find_element(By.NAME, "remove_stage")
+    elem.send_keys(Keys.RETURN)
+
+    time.sleep(1)
+
+def send_invite(driver):
+    elem = driver.find_element(By.XPATH, "//a[4]")
+    elem.click()
+
+    time.sleep(1)
+
+def choose_invite(driver):
+    elem = driver.find_element(By.NAME, "receiver")
+    elem.clear()
+    elem.send_keys("ESEMPIO EMAIL") ##AGGIUNGI EMAIL ESEMPIO A CUI MANDARE INVITO
+
+    elem = driver.find_element(By.NAME, "travel")
+    elem.send_keys("p")
+
+    elem = driver.find_element(By.XPATH, "//a[4]")
+    elem.click()
+
+    time.sleep(1)
+
+def modify_expense(driver):
+    elem = driver.find_element(By.NAME, "send") ##AGGIUNGI NAME send ALL TASTO MANDA INVITO
+    elem.click()
+
+    time.sleep(1)
--- sitoProgettoIsw/organizzatoreViaggi/tests/test_acceptance.py
+++ sitoProgettoIsw/organizzatoreViaggi/tests/test_acceptance.py
@@ -8,7 +8,37 @@ import ta_travel
 driver = webdriver.Firefox()
 driver.get("http://localhost:8000")
 
+##Test creazione sito
 ta_log.login(driver)
 ta_travel.create_travel(driver)
 ta_travel.mytravel(driver)
+ta_log.logout(driver)
+
+##Test modifica viaggio
+ta_log.login(driver)
+ta_travel.mytravel(driver)
+ta_travel.details_travel(driver)
+ta_travel.modify_travel(driver)
+ta_travel.form_modify_travel(driver)
+ta_travel.details_travel(driver)
+ta_travel.modify_travel(driver)
+ta_travel.form_create_location(driver)
+ta_travel.details_travel(driver)
+ta_travel.modify_travel(driver)
+ta_travel.form_delete_location(driver)
+ta_log.logout(driver)
+
+##Test manda inviti
+ta_log.login(driver)
+ta_travel.mytravel(driver)
+ta_travel.details_travel(driver)
+ta_travel.send_invite(driver)
+ta_travel.choose_invite(driver)
+ta_log.logout(driver)
+
+##Test modifica spese
+ta_log.login(driver)
+ta_travel.mytravel(driver)
+ta_travel.details_travel(driver)
+ta_travel.modify_expense(driver)
 ta_log.logout(driver)
