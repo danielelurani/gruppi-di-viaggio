ee4c0f0e2f4806175bad8dc8781bb42304765344
Geraf25 <f.sanna120@studenti.unica.it>
2023-09-27 17:52:31 +0200

    Quasi finiti test di accettazione per errori

--- sitoProgettoIsw/organizzatoreViaggi/tests/ta_travel_error.py
+++ sitoProgettoIsw/organizzatoreViaggi/tests/ta_travel_error.py
@@ -3,17 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
-def mytravel(driver):
-    elem = driver.find_element(By.XPATH, "//a[1]")
-    elem.click()
-
-    time.sleep(1)
-
-def create_travel(driver):
-    elem = driver.find_element(By.NAME, "name")
-    elem.clear()
-    elem.send_keys("Accettazione")
-
+def travel_void(driver):
     elem = driver.find_element(By.NAME, "destination")
     elem.clear()
     elem.send_keys("Destinazione")
@@ -29,118 +19,103 @@ def create_travel(driver):
     elem = driver.find_element(By.NAME, "create_travel")
     elem.send_keys(Keys.RETURN)
 
-    time.sleep(3)
-
-def details_travel(driver):
-    driver.get('http://localhost:8000/organizzatoreViaggi/detailsTravel/24/')
+    ##assert "Compila questo campo" in driver.page_source
 
     time.sleep(3)
 
-def form_modify_travel(driver):
+def travel_wrong_date(driver):
+    elem = driver.find_element(By.NAME, "name")
+    elem.clear()
+    elem.send_keys("Accettazione2")
+
+    elem = driver.find_element(By.NAME, "destination")
+    elem.clear()
+    elem.send_keys("Destinazione2")
+
+    elem = driver.find_element(By.NAME, "start_date")
+    elem.clear()
+    elem.send_keys("2023-04-20")
+
+    elem = driver.find_element(By.NAME, "end_date")
+    elem.clear()
+    elem.send_keys("2023-01-01")
+
+    elem = driver.find_element(By.NAME, "create_travel")
+    elem.send_keys(Keys.RETURN)
+
+    ##assert "La data di fine viaggio non può essere antecedente alla partenza" in driver.page_source
+
+    time.sleep(3)
+
+def modify_travel_void(driver):
     driver.get('http://localhost:8000/organizzatoreViaggi/changeItinerary/24/')
 
     elem = driver.find_element(By.NAME, "name")
     elem.clear()
-    elem.send_keys("Modifica")
 
     elem = driver.find_element(By.NAME, "destination")
     elem.clear()
-    elem.send_keys("Modifica")
 
     elem = driver.find_element(By.NAME, "start_date")
     elem.clear()
-    elem.send_keys("2023-01-10")
 
     elem = driver.find_element(By.NAME, "end_date")
     elem.clear()
-    elem.send_keys("2023-04-02")
 
     elem = driver.find_element(By.NAME, "edit_travel")
     elem.send_keys(Keys.RETURN)
 
     time.sleep(3)
 
-def form_create_location(driver):
+    ##assert "Compila questo campo" in driver.page_source
+
+def add_empty_location(driver):
     driver.get('http://localhost:8000/organizzatoreViaggi/changeItinerary/24/')
 
     elem = driver.find_element(By.NAME, "name_stage")
     elem.clear()
-    elem.send_keys("Tappa")
 
     elem = driver.find_element(By.NAME, "description")
     elem.clear()
-    elem.send_keys("Descrizizone luogo")
 
     elem = driver.find_element(By.NAME, "date")
     elem.clear()
-    elem.send_keys("2023-02-25")
 
     elem = driver.find_element(By.NAME, "add_stage")
     elem.click()
 
     time.sleep(3)
 
-def form_delete_location(driver):
-    driver.get('http://localhost:8000/organizzatoreViaggi/changeItinerary/24/')
+    ##assert "Compila questo campo" in driver.page_source
 
-    elem = driver.find_element(By.NAME, "remove_stage")
-    elem.send_keys(Keys.RETURN)
-
-    time.sleep(3)
-
-def send_invite(driver):
-    driver.get('http://localhost:8000/organizzatoreViaggi/invite')
-
-    time.sleep(3)
-
-def choose_invite(driver):
-    elem = driver.find_element(By.NAME, "receiver")
-    elem.clear()
-    elem.send_keys("daniele@tiscali.it")
-
-    elem = driver.find_element(By.NAME, "travel")
-    elem.send_keys("Ac")
-
+def wrong_invite(driver):
     elem = driver.find_element(By.NAME, "send")
     elem.click()
 
-    elem = driver.find_element(By.XPATH, "//a[1]")
-    elem.click()
+    ##assert "Compila questo campo" in driver.page_source
 
     time.sleep(3)
 
-def modify_expense(driver):
-    elem = driver.find_element(By.NAME, "spese")
+def empty_expense(driver):
+    elem = driver.find_element(By.NAME, "aggiungi")
     elem.click()
+
+    ##assert "Compila questo campo" in driver.page_source
 
     time.sleep(3)
 
-def form_expense(driver):
+def negative_expense(driver):
     elem = driver.find_element(By.NAME, "name")
     elem.clear()
-    elem.send_keys("esempio")
+    elem.send_keys("esempio neg")
 
     elem = driver.find_element(By.NAME, "price")
     elem.clear()
-    elem.send_keys("10.5")
+    elem.send_keys("-5")
 
     elem = driver.find_element(By.NAME, "aggiungi")
     elem.click()
 
-    time.sleep(3)
-
-def remove_expense(driver):
-    elem = driver.find_element(By.NAME, "remove")
-    elem.click()
-
-    time.sleep(3)
-
-def write_comment(driver):
-    elem = driver.find_element(By.ID, "id_content")
-    elem.clear()
-    elem.send_keys("Commento mandato dal test")
-
-    elem = driver.find_element(By.NAME, "commenta")
-    elem.click()
+    ##assert "Il valore deve essere superiore" in driver.page_source
 
     time.sleep(3)
--- sitoProgettoIsw/organizzatoreViaggi/tests/test_acceptance.py
+++ sitoProgettoIsw/organizzatoreViaggi/tests/test_acceptance.py
@@ -4,11 +4,12 @@ from selenium.webdriver.common.by import By
 
 import ta_log
 import ta_travel
+import ta_travel_error
 
 driver = webdriver.Firefox()
 driver.get("http://localhost:8000")
 
-##Test creazione sito
+##Test creazione viaggio
 ta_log.login(driver)
 ta_travel.create_travel(driver)
 ta_travel.mytravel(driver)
@@ -47,4 +48,27 @@ ta_log.login(driver)
 ta_travel.mytravel(driver)
 ta_travel.details_travel(driver)
 ta_travel.write_comment(driver)
+ta_log.logout(driver)
+
+##Test creazione viaggio errato
+ta_log.login(driver)
+ta_travel_error.travel_void(driver)
+ta_travel_error.travel_wrong_date(driver)
+ta_log.logout(driver)
+
+##Test modifica viaggio errata
+ta_log.login(driver)
+ta_travel.mytravel(driver)
+ta_travel.details_travel(driver)
+ta_travel_error.modify_travel_void(driver)
+ta_travel_error.add_empty_location(driver)
+ta_log.logout(driver)
+
+##Test modifica spese errate
+ta_log.login(driver)
+ta_travel.mytravel(driver)
+ta_travel.details_travel(driver)
+ta_travel.modify_expense(driver)
+ta_travel_error.empty_expense(driver)
+ta_travel_error.negative_expense(driver)
 ta_log.logout(driver)
